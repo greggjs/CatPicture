992da54f59779cd21f79e31f4c43674a76b21ef2
Jake Gregg <greggjs@muohio.edu>
2012-08-23 10:39:16 -0400

    Brightness Changes
    Changed some code so that the brightness cycles

--- src/CatPictureApp.cpp
+++ src/CatPictureApp.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file CatPictureApp.cpp
+ * A simple app to display a cat picture using Cinder
+ *
+ * @author Jake Gregg
+ * @date 2012-08-23
+ *
+ * @note This file is (c) 2012. It is licensed under the
+ * CC by 3.0 License, which means you are free to use, share,
+ * and remix it as long as you give attribution. Commercial uses
+ * are allowed.
+ */
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
 
@@ -11,10 +24,15 @@ class CatPictureApp : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+
+	/// Brightness ranges from 0 to 1
+  private:
+	float brightness_;
 };
 
 void CatPictureApp::setup()
 {
+	brightness_ = 1.0f;
 }
 
 void CatPictureApp::mouseDown( MouseEvent event )
@@ -23,12 +41,16 @@ void CatPictureApp::mouseDown( MouseEvent event )
 
 void CatPictureApp::update()
 {
+	brightness_ = brightness_ - 0.01f;
+	if (brightness_ < 0.0f) {
+		brightness_ = 1.0f;
+	}
 }
 
 void CatPictureApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( brightness_, brightness_, brightness_ ) ); 
 }
 
 CINDER_APP_BASIC( CatPictureApp, RendererGl )
